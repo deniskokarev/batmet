f7417f3e6ca3d2bcf571f6a2f8e5b6aa8a5c7038
Denis Kokarev <d_kokarev@mail.ru>
2023-06-11 18:21:23 -0400

    trying to make leds work (no dice yet)

--- zfirm/src/main.c
+++ zfirm/src/main.c
@@ -1,12 +1,23 @@
 #include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
+
 LOG_MODULE_REGISTER(batmet, 4);
 
+static const struct gpio_dt_spec red_led = GPIO_DT_SPEC_GET(DT_ALIAS(ledr), gpios);
+static const struct gpio_dt_spec green_led = GPIO_DT_SPEC_GET(DT_ALIAS(ledg), gpios);
+
 int main(void) {
+	if (!gpio_is_ready_dt(&red_led) || !gpio_is_ready_dt(&green_led)) {
+		return -1;
+	}
+	int led_state = 1;
 	while (1) {
 		LOG_INF("Hello World! %s", CONFIG_ARCH);
-		k_sleep(K_MSEC(10000));
+		gpio_pin_set_dt(&red_led, led_state);
+		gpio_pin_set_dt(&green_led, !led_state);
+		led_state = !led_state;
+		k_sleep(K_MSEC(5000));
 	}
 	return 0;
 }
