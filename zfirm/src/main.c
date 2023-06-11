c8ce6c81db7c50922b67d3ebd08e0fab07b7d587
Denis Kokarev <d_kokarev@mail.ru>
2023-06-11 18:53:44 -0400

    led works on Zephyr now!!!

--- zfirm/src/main.c
+++ zfirm/src/main.c
@@ -2,16 +2,23 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(batmet, 4);
+LOG_MODULE_REGISTER(batmet, LOG_LEVEL_DBG);
 
 static const struct gpio_dt_spec red_led = GPIO_DT_SPEC_GET(DT_ALIAS(ledr), gpios);
 static const struct gpio_dt_spec green_led = GPIO_DT_SPEC_GET(DT_ALIAS(ledg), gpios);
 
 int main(void) {
 	if (!gpio_is_ready_dt(&red_led) || !gpio_is_ready_dt(&green_led)) {
+		LOG_ERR("gpio_is_ready_dt() error");
 		return -1;
 	}
-	int led_state = 1;
+	if (gpio_pin_configure_dt(&red_led, GPIO_OUTPUT_ACTIVE) < 0 ||
+	    gpio_pin_configure_dt(&green_led, GPIO_OUTPUT_ACTIVE) < 0) {
+		LOG_ERR("gpio_pin_configure_dt() error");
+		return -2;
+	}
+
+	int led_state = 0; // start with green
 	while (1) {
 		LOG_INF("Hello World! %s", CONFIG_ARCH);
 		gpio_pin_set_dt(&red_led, led_state);
