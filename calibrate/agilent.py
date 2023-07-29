50ac2c2db597236cfd10ca1c4b2190121b953a63
Denis Kokarev <d_kokarev@mail.ru>
2023-07-29 14:04:35 -0400

    enveloped Multimeter operations into a class

--- calibrate/agilent.py
+++ calibrate/agilent.py
@@ -7,50 +7,69 @@ import serial.tools.list_ports
 from serial import PARITY_NONE, STOPBITS_ONE
 
 
-def open_multimeter(port: Optional[str] = None) -> serial.SerialBase:
+class Mult:
     """
-    open Agilent 34401A multimeter port
+    open Agilent 34401A multimeter
     using:
     - null-modem cable: https://www.amazon.com/dp/B00006B8BJ
     - Prolific PL2303 Chip, DB9 Adapter: https://www.amazon.com/dp/B00GRP8EZU
     Make sure to set the RS232 interface with 9600 baud 8n1 mode
     """
-    if not port:
-        ports = [pi for pi in serial.tools.list_ports.comports() if pi.pid == 0x2303 and pi.vid == 0x067b]
-        if len(ports) < 1:
-            raise Exception("no usb serial devices detected")
-        elif len(ports) > 1:
-            raise Exception("more than one device detected, please specify port")
-        port = ports[0]
-    ser = serial.Serial(port=port.device,
-                        baudrate=9600,
-                        timeout=1.0,
-                        parity=PARITY_NONE,
-                        stopbits=STOPBITS_ONE,
-                        )
-    try:
-        ser.write(b"*IDN?\r\n")
-        ser.flush()
-        name = ser.read_until().decode('ascii').rstrip()
-        if not re.search(r"34401A", name):
-            raise Exception("This is not 34401A multimeter")
+    PID = 0x2303
+    VID = 0x067b
+    MODEL = "34401A"
+
+    def __init__(self, port: Optional[str] = None):
+        self.ser = Mult.open_serial(port)
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        Mult.close_serial(self.ser)
+
+    @classmethod
+    def open_serial(cls, port: Optional[str] = None) -> serial.SerialBase:
+        if not port:
+            ports = [pi for pi in serial.tools.list_ports.comports() if pi.pid == cls.PID and pi.vid == cls.VID]
+            if len(ports) < 1:
+                raise Exception("no serial devices detected")
+            elif len(ports) > 1:
+                raise Exception("more than one serial device detected, please specify port")
+            port = ports[0]
+        ser = serial.Serial(port=port.device,
+                            baudrate=9600,
+                            timeout=1.0,
+                            parity=PARITY_NONE,
+                            stopbits=STOPBITS_ONE,
+                            )
+        try:
+            ser.write(b"*IDN?\r\n")
+            ser.flush()
+            name = ser.read_until().decode('ascii').rstrip()
+            if not re.search(cls.MODEL, name):
+                raise Exception(f"There is no {cls.MODEL} multimeter connected to {port}")
+            ser.write(b"*CLS\r\n")
+            ser.write(b"SYSTEM:REMOTE\r\n")
+            ser.write(b'DISPLAY:TEXT "EXEC..."\r\n')
+            return ser
+        except Exception as e:
+            ser.close()
+            raise e
+
+    @staticmethod
+    def close_serial(ser: serial.SerialBase) -> None:
+        ser.write(b"SYSTEM:LOCAL\r\n")
         ser.write(b"*CLS\r\n")
-        ser.write(b"SYSTEM:REMOTE\r\n")
-        ser.write(b'DISPLAY:TEXT "EXEC: 0"\r\n')
-        return ser
-    except Exception as e:
+        ser.flush()
         ser.close()
-        raise e
 
 
 def demo() -> int:
     try:
-        with open_multimeter() as ser:
+        with Mult() as mult:
             print("ok")
             time.sleep(3)
-            ser.write(b"SYSTEM:LOCAL\r\n")
-            ser.write(b"*CLS\r\n")
-            ser.flush()
         return 0
     except Exception as e:
         print(f"Error: {e}", file=sys.stderr)
