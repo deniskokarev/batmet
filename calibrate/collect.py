806aa81c1cbbe98ebdf39219fedecb0376cedf0a
Denis Kokarev <d_kokarev@mail.ru>
2023-07-29 18:24:48 -0400

    made calibration range [450..4100) or until 3A

--- calibrate/collect.py
+++ calibrate/collect.py
@@ -11,9 +11,9 @@ from batmet import Batmet
 def collect(mult: Mult, bm: Batmet) -> None:
     """
     Run the collection loop
-    Go from d=0..4095 or until 2.5A current is met
+    Go from d=[450..4090] or until 3A current is met
     """
-    for d in [i * 10 for i in range(410)]:
+    for d in range(450, 4100, 10):
         bm.set_d(d)
         mult.display(f"D={d}")
         # reading each measurement few times
@@ -23,7 +23,7 @@ def collect(mult: Mult, bm: Batmet) -> None:
             vs += mult.dc_current()
         va = vs / cnt
         print(f"{d}\t{va}", flush=True)
-        if va > 2.5:
+        if va > 3:
             break
 
 
