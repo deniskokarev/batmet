52e75b10229886c8f0d27e4efde145fa65a58513
Denis Kokarev <d_kokarev@mail.ru>
2023-07-29 14:52:07 -0400

    print current reading from multimeter

--- calibrate/agilent.py
+++ calibrate/agilent.py
@@ -4,7 +4,7 @@ import time
 from typing import Optional
 
 import serial.tools.list_ports
-from serial import PARITY_NONE, STOPBITS_ONE
+from serial import PARITY_NONE
 
 
 class Mult:
@@ -23,7 +23,7 @@ class Mult:
         self.ser = Mult.open_serial(port)
 
     def __enter__(self):
-        pass
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         Mult.close_serial(self.ser)
@@ -39,9 +39,9 @@ class Mult:
             port = ports[0]
         ser = serial.Serial(port=port.device,
                             baudrate=9600,
-                            timeout=1.0,
+                            timeout=2.0,
                             parity=PARITY_NONE,
-                            stopbits=STOPBITS_ONE,
+                            stopbits=2,
                             )
         try:
             ser.write(b"*IDN?\r\n")
@@ -52,24 +52,49 @@ class Mult:
             ser.write(b"*CLS\r\n")
             ser.write(b"SYSTEM:REMOTE\r\n")
             ser.write(b'DISPLAY:TEXT "EXEC..."\r\n')
+            ser.flush()
+            time.sleep(.3)
             return ser
         except Exception as e:
             ser.close()
             raise e
 
     @staticmethod
+    def get_err(ser: serial.SerialBase) -> None:
+        ser.write(b"SYST:ERR?\r\n")
+        ser.flush()
+        return ser.read_until().decode('ascii').rstrip()
+
+    @staticmethod
     def close_serial(ser: serial.SerialBase) -> None:
+        print(f"err={Mult.get_err(ser)}")
         ser.write(b"SYSTEM:LOCAL\r\n")
         ser.write(b"*CLS\r\n")
         ser.flush()
         ser.close()
 
+    def _read_float(self) -> float:
+        result = self.ser.read_until().decode('ascii').rstrip()
+        if len(re.findall(r"[+-][0-9][.][0-9]*[Ee][+-][0-9][0-9]", result)) >= 1:
+            return float(result)
+        else:
+            raise Exception("Couldn't read a floating point number")
+
+    def dc_current(self) -> float:
+        self.ser.write(b":MEAS:CURR:DC?\r\n")
+        return self._read_float()
+
+    def dc_voltage(self) -> float:
+        self.ser.write(b":MEAS:VOLT:DC?\r\n")
+        return self._read_float()
+
 
 def demo() -> int:
     try:
         with Mult() as mult:
-            print("ok")
-            time.sleep(3)
+            for i in range(3):
+                print(f"I={mult.dc_current()}")
+                time.sleep(1)
         return 0
     except Exception as e:
         print(f"Error: {e}", file=sys.stderr)
